d6e0f7a46235c0adf80a959b44b73c27ace8d331
RyanNekadio <ryanekadio12@hotmail.co.uk>
2023-12-12 14:41:21 +0000

    Created homepage and added css

--- frontend_project_client/src/components/Home.js
+++ frontend_project_client/src/components/Home.js
@@ -1,16 +1,30 @@
 import { Link, Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import logo from "../logo.svg";
 
 const Home = () => {
     const location = useLocation();
-
+ 
     const renderHomePage = location.pathname === '/';
     return (
     <>
         {renderHomePage && (
         <>
-            <h1>Home Page</h1>
-            <p><Link to="/question">Start Quiz</Link></p>
+            <header className="homepage">Which Trainer Are You?</header>
+            <hr/>
+            <section className="main-homepage">
+                <h1 className="home">BNTA TRAINER QUIZ</h1>
+                <img className="App-logo" src={logo} alt="Image of trainers"/>
+                <p>Discover your BNTA trainer match with our interactive quiz!</p>
+                <p>Enter your username and click start:</p>
+                <form id="homepage-form">
+                    <input 
+                        placeholder="Enter a username" 
+                        type="text"
+                    />
+                    <button><Link to="/question">Start quiz</Link></button>
+                </form>
+            </section>
          </>
         )}
         <Outlet />
