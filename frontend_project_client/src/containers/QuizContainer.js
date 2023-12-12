fd45d52dd7f102cc5ff4c936abfc2153dd54258c
SamRGLee <sam_greenfield_lee@hotmail.co.uk>
2023-12-12 10:49:59 +0000

    created fetch request and useStates in the container

--- frontend_project_client/src/containers/QuizContainer.js
+++ frontend_project_client/src/containers/QuizContainer.js
@@ -1,8 +1,30 @@
 import Question from "../components/Question";
 import ResultsPage from "../components/ResultsPage";
+import { useState, useEffect } from "react";
 
 
 const QuizContainer = () => {
+    const [stateUserName, setStateUserName] = useState("")
+    const [currentQResponse, setCurrentQResponse] = useState(null) //null as opposed to false bc a is fed int as a response
+    const [qAnswered, setQAnswered] = useState([])
+    const [listOfQs, setListOfQs] = useState([])
+    const [quizFinished, setQuizFinished] = useState(null)
+
+    const loadQuizData = async () => {
+        const response = await fetch (`http://localhost:8080/quizzes/random?userName=${stateUserName}&numberOfQuestions=3`, 
+        {
+            method: "POST",
+            headers: {"Content-Type": "application/json"}
+        })
+        const quizData = await response.json()
+        console.log(quizData)
+
+
+    }
+    useEffect(() => {
+        loadQuizData()
+    }, [])
+
     return (  
         <>
             <h1>BNTA Trainers Quiz!</h1>
