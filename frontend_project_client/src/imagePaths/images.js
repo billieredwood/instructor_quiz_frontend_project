9d4da140891a7db0e7b89d4fdef337f37b24c35f
SamRGLee <sam_greenfield_lee@hotmail.co.uk>
2023-12-14 19:10:20 +0000

    testing

---
+++ frontend_project_client/src/imagePaths/images.js
@@ -0,0 +1,15 @@
+// src/images/index.js
+
+// Assume you have image files named "image1.jpg", "image2.jpg", etc.
+const annaImages = [
+    require('../AnnaImages/AnnaMain.png'),
+    require('../AnnaImages/Anna1.jpeg'), // Use the `require` function to get the image path
+    require('../AnnaImages/Anna2.jpeg'),
+    require('../AnnaImages/AnnaDog1.jpeg'),
+    require('../AnnaImages/AnnaDog2.jpeg'),
+    require('../AnnaImages/AnnaDog3.jpeg')
+    // Add more image paths as needed
+  ];
+  
+  export default imagePaths;
+  
